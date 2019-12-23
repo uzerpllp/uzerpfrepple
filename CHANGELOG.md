c35e32153f01428ca7fca3a6721a254ea3e52424
Steve Blamey <steve.blamey@adbury.net>
2019-12-23 14:00:58 +0000

    Bump release

---
+++ CHANGELOG.md
@@ -0,0 +1,9 @@
+# Change Log
+
+All notable changes to this project will be documented in this file.
+
+## 1.1
+
+### Changed
+
+- Removed suboperations import (deprecated in frePPLe release 6.0.0)
--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="uzerpfrepple",
-    version="1.0",
+    version="1.1",
     author="uzERP LLP",
     author_email="sblamey@uzerp.com",
     description="Frepple ERP connector for uzERP",
