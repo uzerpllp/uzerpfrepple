d78bba4d21d0343920330df3c52d89586ec495cf
Steve Blamey <steve.blamey@adbury.net>
2021-02-24 15:03:21 +0000

    Update version

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="uzerpfrepple",
-    version="1.2",
+    version="1.3",
     author="uzERP LLP",
     author_email="sblamey@uzerp.com",
     description="Frepple ERP connector for uzERP",
